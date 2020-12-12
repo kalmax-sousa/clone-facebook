816b043e26cd11ee6f727de41ba200d577ac192d
kalmax-sousa <kalmax.santos.123@gmail.com>
2020-12-12 12:41:04 -0300

    ToolBar finalizada/Avatar finalizado

--- components/ToolBar/index.jsx
+++ components/ToolBar/index.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Container, Row } from './styles';
+import { Container, Row, Input, Divider, Menu, MenuText, Separator } from './styles';
+import Avatar from '../Avatar';
+import {Ionicons, MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons'
+
+const userAvatar = require('../../assets/user.png')
 
 const ToolBar = () => {
     return(
         <Container>
             <Row>
+                <Avatar source={userAvatar}/>
+                <Input placeholder="O que você está pensando" />
+            </Row>
+            
+            <Divider />
+            
+            <Row>
+                <Menu>
+                    <Ionicons name="ios-videocam" size={22} color='#f44'/>
+                    <MenuText>Ao Vivo</MenuText>
+                </Menu>
+
+                <Separator />
+
+                <Menu>
+                    <MaterialIcons name="photo-size-select-actual" size={20} color='#4caf50'/>
+                    <MenuText>Foto</MenuText>
+                </Menu>
+
+                <Separator />
+
+                <Menu>
+                    <MaterialCommunityIcons name="video-plus" size={22} color='#e141fc'/>
+                    <MenuText>Sala</MenuText>
+                </Menu>
             </Row>
         </Container>
     );
--- components/ToolBar/styles.jsx
+++ components/ToolBar/styles.jsx
@@ -9,5 +9,38 @@ export const Row = styled.View`
     align-items: center;
     background: #ffffff;
     flex-direction: row;
+    padding: 0 11px;
     width: 100%;
+`;
+
+export const Input = styled.TextInput`
+    height: 50px;
+    padding: 0 8px;
+    width: 100%;
+`;
+
+export const Divider = styled.View`
+    background: #f0f0f0;
+    height: 0.5px;
+    width: 100%;
+`;
+
+export const Menu = styled.View`
+    align-items: center;
+    flex: 1;
+    flex-direction: row;
+    height: 42px;
+    justify-content: center;
+`;
+
+export const MenuText = styled.Text`
+    font-size: 12px;
+    font-weight: 500;
+    padding-left: 11px;
+`;
+
+export const Separator = styled.View`
+    background: #f0f0f0;
+    height: 26px;
+    width: 1px;
 `;
