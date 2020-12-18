ef1e5b57297c2c3fbfd97f055259e43b585e586b
kalmax-sousa <kalmax.santos.123@gmail.com>
2020-12-18 20:44:28 -0300

    Inicializando Feed

---
+++ components/Feed/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+
+import { Container } from './styles';
+
+const Feed = () => {
+  return (
+    <>
+      <Container>
+
+      </Container>
+    </>
+  );
+}
+
+export default Feed;
